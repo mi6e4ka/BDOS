86628791a4daf57dfbfcf3d903d87a5dfd2e4458
Миха Liven <admin@1636.xn--p1ai>
2021-05-02 23:01:38 +0300

    alpha 02052021.1

---
+++ scripts/clock.js
@@ -0,0 +1,13 @@
+function clockUpdate() {
+	console.log('updating');
+	var now = new Date().toLocaleString("ru", {month:'long',day:'numeric',weekday:'long',hour:'numeric',minute:'numeric',second:'numeric'}).split(', ');
+	document.getElementsByClassName('hours')[0].innerHTML = now[2].split(':')[0];
+	document.getElementsByClassName('minuts')[0].innerHTML = now[2].split(':')[1];
+	document.getElementsByClassName('hours')[1].innerHTML = now[2].split(':')[0];
+	document.getElementsByClassName('minuts')[1].innerHTML = now[2].split(':')[1];
+	document.getElementById('seconds').innerHTML = now[2].split(':')[2];
+	document.getElementById('weekday').innerHTML = now[0];
+	document.getElementById('monthday').innerHTML = now[1];
+}
+clockUpdate();
+setInterval(clockUpdate, 1000);
