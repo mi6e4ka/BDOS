f17f29811b7eea13d27abca6e3905f812b59b919
404 <nacadyi.gd@gmail.com>
2022-05-01 23:02:36 +0300

    new version 01052022
    Мир. Труд. Май.
    cursor update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# BDOS v29042022
+# BDOS v01052022
+[![wakatime](https://wakatime.com/badge/user/99cb8241-f2fc-4e1e-b596-ef998a5e6b4c/project/df411d28-2f55-434c-af63-e234d67337c0.svg)](https://wakatime.com/badge/user/99cb8241-f2fc-4e1e-b596-ef998a5e6b4c/project/df411d28-2f55-434c-af63-e234d67337c0)  
 Browser Diskless Operation System
 - This is VERY unstable version, please use beta or stable branch
 - Test: http://bdos.tk (only beta)
 - BDOS app API: https://github.com/mi6e4ka/webOS/wiki/Документация-по-jxe-файлам
- 
--- scripts/cursor.js
+++ scripts/cursor.js
@@ -3,23 +3,46 @@ container = document.getElementById('osContainer')
 
 var oldX = cursor.style.left;
 var oldY = cursor.style.top;
-var offsetX = Math.round(container.getBoundingClientRect().left)
-var offsetY = Math.round(container.getBoundingClientRect().top)
+var offsetX = Math.round(container.getBoundingClientRect().left);
+var offsetY = Math.round(container.getBoundingClientRect().top);
+var oldOver = document;
+var clickEvent = document.createEvent('MouseEvents');
 
 document.getElementById('osContainer').addEventListener('mousemove', e => {
-  cursor.style.top = oldY;
-  cursor.style.left = oldX;
-  oldX = Number(oldX + e.movementX);
-  oldY = Number(oldY + e.movementY);
+  async function move(){
+    cursor.style.top = oldY;
+    cursor.style.left = oldX;
+    oldX = Number(oldX + e.movementX);
+    oldY = Number(oldY + e.movementY);
+  }
+  move()
+  async function update(){
+    clickEvent.initEvent('mouseout', true, true);
+    oldOver.dispatchEvent(clickEvent);
+    clickEvent.initEvent('mouseover', true, true);
+    newOver.dispatchEvent(clickEvent);
+    //console.log('mouseover', newOver)
+    //console.log('mouseout', oldOver)
+    oldOver = newOver
+  }
+  newOver = document.elementFromPoint(oldX+offsetX, oldY+offsetY);
+  if (newOver != oldOver && newOver.id != 'cursor.img') {update()}
 });
 
 container.onclick = (e) => {
-  container.requestPointerLock();
-  if (e.which = 1){
-    var clickEvent = document.createEvent('MouseEvents');
-    clickEvent.initEvent('click', true, true);
-    cursor.style.display = 'none';
-    document.elementFromPoint(oldX+offsetX, oldY+offsetY).dispatchEvent(clickEvent);
+  async function click(){
+    container.requestPointerLock();
     cursor.style.display = 'block';
+    if (e.which = 1){
+      clickEvent.initEvent('click', true, true);
+      cursor.style.display = 'none';
+      document.elementFromPoint(oldX+offsetX, oldY+offsetY).dispatchEvent(clickEvent);
+      cursor.style.display = 'block';
+    }
   }
+  click()
+}
+
+container.ondrag = (e) => {
+  alert(123)
 }
